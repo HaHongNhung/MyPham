d1bbc9c91dc0710f33fdf7821e5bee435d04077c
huychet004 <huychet004@gmail.com>
2024-11-02 15:39:26 +0700

    fix users

--- backend/DuAnThucTap/routes/client/auth/authRoute.js
+++ backend/DuAnThucTap/routes/client/auth/authRoute.js
@@ -5,13 +5,11 @@ const authController = require('../../../controllers/Client/authController');
 const router = express.Router();
 
 router.post('/login', authController.login);
-router.post('/register', authController.register); // Thêm route cho đăng ký
+router.post('/register', authController.register); 
 
 
-// Thêm route cho yêu cầu đặt lại mật khẩu
 router.post('/request-password-reset', authController.requestPasswordReset);
 
-// Thêm route cho đặt lại mật khẩu
 router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
