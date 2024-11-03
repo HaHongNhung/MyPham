0dc27878625c65563ba6f4394385b948a74156ef
Huuduy2004 <duynguyenhuu2004@gmail.com>
2024-11-03 13:20:17 +0700

    update

--- backend/DuAnThucTap/routes/index.js
+++ backend/DuAnThucTap/routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 const orderRoute = require('../routes/orders/index');
 const orderItemRoute = require('../routes/orderItems/index');
 
-/* GET home page. */
 router.get('/get', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
