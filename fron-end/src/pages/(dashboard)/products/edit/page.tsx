ead90e04ecd29db75ae6cec217dd9d5bf8d7d444
hanvph35034 <sonyeuthunhem@gmail.com>
2024-11-06 06:05:01 +0700

    base fe

---
+++ fron-end/src/pages/(dashboard)/products/edit/page.tsx
@@ -0,0 +1,20 @@
+import instance from "@/configs/axios";
+import { BackwardFilled, Loading3QuartersOutlined } from "@ant-design/icons";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button, Form, FormProps, Input, message } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import { Link, useParams } from "react-router-dom";
+
+
+
+const ProductEditPage = () => {
+    
+    return (
+       <>
+       <div className="">
+        </div></>
+    );
+};
+
+export default ProductEditPage;
+
